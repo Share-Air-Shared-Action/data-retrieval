ca861a23223d21fd7d862d3b6b3dc8c67f014432
George W. Walker <george@georgewwalker.com>
2017-07-25 18:49:46 -0500

    Update Met One CURL script
    Comment, add logging, and general fixes. Also updated crontab to use the new script.

--- metone/curlmetone.sh
+++ metone/curlmetone.sh
@@ -1,19 +1,47 @@
+# This file downloads the data for Met One devices
+# Gets the compIds from the provided arguments
+# e.x. curlmetone.sh 12396 12397 12398
+
+# Set the path of the output directory
+outputDir="/data/sasa_airquality/metone/"
+
+# Set the name of the log file
+logFile="curlmetone.log"
+
+# Check if arguments supplied
+if [ $# -eq 0 ]
+  then
+    # Set the error text
+    error="[$(date)] No compIds supplied. Please supply compIds (e.x. curlmetone.sh 12396 12397 12398)."
+
+    # Send the error to stderr
+    echo $error 1>&2
+
+    # Append the error to the log file.
+    echo $error >> $outputDir$logFile
+    exit 1
+fi
+
+# Redirect all output to a log file
+exec &>> $outputDir$logFile
+
 # Get API key
 . ./apikey.sh
 
+# Set the start and end date
 end_date=$(date)
-start_date= date -d "- 1 days"
+start_date=$(date -d "- 1 days")
 
-#start_date= date -d '2017-06-04'
-#end_date= date -d '2017-06-09'
-#echo $end_date
-echo $start_date
-echo $end_date
+# Convert the date to Milliseconds since Epoch as required by Met One API
 startms=$(date "+%s%3N" -d "$start_date")
 endms=$(date "+%s%3N" -d "$end_date")
-echo $startms
-echo $endms
-#3 metone jsons
-curl "https://www.grovestreams.com/api/comp/W12396/feed?sd=$startms&ed=$endms&retStreamId&api_key=$apikey" > /data/sasa_airquality/metone/12396.json
-curl "https://www.grovestreams.com/api/comp/W12397/feed?sd=$startms&ed=$endms&retStreamId&api_key=$apikey" > /data/sasa_airquality/metone/12397.json
-curl "https://www.grovestreams.com/api/comp/W12398/feed?sd=$startms&ed=$endms&retStreamId&api_key=$apikey" > /data/sasa_airquality/metone/12398.json
+
+
+echo "Downloading data from Met One devices between $start_date and $end_date."
+echo "Calculated milliseconds since epoch. Start: $startms End: $endms"
+
+for compId in "$@"
+do
+  echo "Downloading data for compId $compId..."
+  curl "https://www.grovestreams.com/api/comp/W$compId/feed?sd=$startms&ed=$endms&retStreamId&api_key=$apikey" > "$outputDir$compId.json"
+done
