a327a72cc85e7af9313152ce4dfe0327214c13fd
George W. Walker <george@georgewwalker.com>
2017-07-25 01:27:01 -0500

    Commented, added fix for added column on Metone table
    Read through and figured out code, added comments to describe what it does. Added additional column to row processing of CSV to handle the additional column added to the database for the community.
    
    Added some debugging messages and code comments to the shell script that loads Met One data.

--- metone/metoneload.sh
+++ metone/metoneload.sh
@@ -1,9 +1,17 @@
+#!/bin/bash
+
+# This helps with debugging
+set -x #echo on
+
+# Run a Python script to convert the downloaded JSON to CSV.
 python /data/sasa_airquality/metone/json_to_csv.py : /data/sasa_airquality/metone/12396.json /data/sasa_airquality/metone/12396.csv
 python /data/sasa_airquality/metone/json_to_csv.py : /data/sasa_airquality/metone/12397.json /data/sasa_airquality/metone/12397.csv
 python /data/sasa_airquality/metone/json_to_csv.py : /data/sasa_airquality/metone/12398.json /data/sasa_airquality/metone/12398.csv
 
+# Run a Python script to process the converted CSV to change data types and add columns before import.
 python /data/sasa_airquality/metone/makedata.py /data/sasa_airquality/metone/12396.csv
 python /data/sasa_airquality/metone/makedata.py /data/sasa_airquality/metone/12397.csv
 python /data/sasa_airquality/metone/makedata.py /data/sasa_airquality/metone/12398.csv
 
-#pgloader /data/sasa_airquality/metone/metload.data
+# Load the CSVs into the database.
+pgloader /data/sasa_airquality/metone/metload.data
