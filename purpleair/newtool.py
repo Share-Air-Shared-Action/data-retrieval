0119d415aac80b1e2131b00257677c024ce31da4
George W. Walker <george@georgewwalker.com>
2017-07-25 20:57:05 -0500

    Added section to write pgloader file

--- purpleair/newtool.py
+++ purpleair/newtool.py
@@ -15,6 +15,9 @@ decoded_response = response.decode('iso-8859-1').encode('utf8')
 # Parse the JSON
 data = json.loads(decoded_response)
 
+# Open and clear the pgloader file
+pgloaderfile = open("/data/sasa_airquality/purpleair/downloads/pgloaderfile.load","w")
+
 # For each "result" in the JSON
 for result in data['results']:
     # If the label starts with SASA
@@ -28,3 +31,9 @@ for result in data['results']:
 
         # Call the shell script to download the data
         call(["sh", "/data/sasa_airquality/purpleair/curltool.sh", thingspeak_id, thingspeak_apikey, outputfilename])
+
+        # Write a section of the pgloader file for this item
+        pgloaderfile.write("LOAD CSV\n    FROM 'new" + outputfilename + ".csv' WITH ENCODING iso-646-us\n    INTO postgresql://postgres@localhost:5432/sasa_airquality?purpleairprimary\n    WITH  skip header = 2,\n        fields optionally enclosed by '\"',\n        fields escaped by backslash-quote,\n        fields terminated by ','\n\n    SET work_mem to '32 MB', maintenance_work_mem to '64 MB';\n\n")
+
+# Close the pgloaderfile
+pgloaderfile.close()
