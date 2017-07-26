bcb18ee1d6c8f1aa3c324fb99d045b947a732467
George W. Walker <george@georgewwalker.com>
2017-07-25 20:57:32 -0500

    Commented and fixed

--- purpleair/purpleairload.sh
+++ purpleair/purpleairload.sh
@@ -1,12 +1,20 @@
+#!/bin/bash
 
+# This file processes the downloaded files and puts them into the database
 
-for file in /data/sasa_airquality/purpleair/SASA_PA*
+# Set the path of the input directory
+inputDir="/data/sasa_airquality/purpleair/downloads"
+
+# For each CSV in the input directory
+for file in $inputDir/SASA*.csv
 do
-  python3 /data/sasa_airquality/purpleair/ijsontest.py "$file" 
+  # Run the parser on the file
+  python3 /data/sasa_airquality/purpleair/parseCSV.py "$file"
 done
 
-pgloader /data/sasa_airquality/purpleair/data.load
-
-
-#after load remove all csv files.
-rm /data/sasa_airquality/purpleair/*csv
+# For each parsed CSV
+for file in $inputDir/newSASA*.csv
+do
+  # Run the import command on the file
+   pgloader --type csv --with "skip header = 2" --with "fields fields optionally enclosed by '\"'" --with "fields escaped by backslash-quote" --with "fields terminated by ','" "$file" postgresql://postgres@localhost:5432/sasa_airquality?purpleairprimary
+done
