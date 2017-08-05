42c84c47095bcccd692bb6549ee44072ee545d4b
George W. Walker <george@georgewwalker.com>
2017-08-05 07:21:35 -0500

    Move logging to airbeamterrier python script
    The carriage returns on the database entries were not working correctly by redirecting. This is better anyway.

--- airbeamterrier/airbeamterrier.py
+++ airbeamterrier/airbeamterrier.py
@@ -1,8 +1,18 @@
 # This file gets a list of sessions from the Aircasting API, then downloads the JSON for each session from the API, parses it and puts it into the database
 
-import json, urllib, sys, psycopg2
+import json, urllib, sys, os, psycopg2
 from datetime import datetime
 import keys
+
+# Open the log file
+f = open("/data/sasa_airquality/airbeamterrier/logs/airbeamterrier.log", "a+")
+
+# Create a function for logging/displaying output
+def debugMessage(message):
+    print(message)
+    f.write(message + "\n")
+    f.flush()
+    os.fsync(f.fileno())
 
 # Set the "usernames" for the API call
 usernames = ["SASA_AB1", "SASA_AB2", "SASA_AB3", "SASA_AB4", "SASA_AB5", "SASA_AB6"]
@@ -16,7 +26,7 @@ yesterday_day_of_year = today_day_of_year - 1
 for username in usernames:
     allSessionsURL = "http://aircasting.org/api/sessions.json?page=0&page_size=50&q%5Btime_from%5D=0&q%5Btime_to%5D=2359&q%5Bday_from%5D=" + str(yesterday_day_of_year) + "&q%5Bday_to%5D=" + str(today_day_of_year) + "&q%5Busernames%5D=" + username + "&q%5Blocation%5D=Chicago&q%5Bunit_symbol%5D=%C2%B5g%2Fm"
 
-    print("[" + str(datetime.now()) + "] Requesting sessions for " + username + " from day " + str(yesterday_day_of_year) + " to " + str(today_day_of_year))
+    debugMessage("[" + str(datetime.now()) + "] Requesting sessions for " + username + " from day " + str(yesterday_day_of_year) + " to " + str(today_day_of_year))
 
     try:
         # Get the JSON from the URL
@@ -26,7 +36,7 @@ for username in usernames:
         allSessionsData = json.loads(allSessionsResponse)
 
         # Debugging message
-        print("[" + str(datetime.now()) + "] Got " + str(len(allSessionsData)) + " sessions.")
+        debugMessage("[" + str(datetime.now()) + "] Got " + str(len(allSessionsData)) + " sessions.")
 
         sessionCount = 1
 
@@ -39,8 +49,10 @@ for username in usernames:
             # Time to request the JSON for the specific ID
             sessionURL = "http://aircasting.org/api/sessions/" + str(currentID) + ".json"
 
-            print("[" + str(datetime.now()) + "] [" + str(sessionCount) + " of " + str(len(allSessionsData)) + "] Requesting data from session " + str(currentID) + " of " + username)
+            debugMessage("[" + str(datetime.now()) + "] [" + str(sessionCount) + " of " + str(len(allSessionsData)) + "] Requesting data from session " + str(currentID) + " of " + username)
+
             sessionCount += 1
+
             try:
                 # Get the JSON from the URL
                 currentSessionResponse = urllib.urlopen(sessionURL).read()
@@ -50,7 +62,8 @@ for username in usernames:
 
                 # Set the session title
                 session_title = currentSessionData['title']
-                print("[" + str(datetime.now()) + "] Session data found. Session title: " + session_title)
+
+                debugMessage("[" + str(datetime.now()) + "] Session data found. Session title: " + session_title)
 
                 # For each stream within the current session
                 for streamname, streamdata in currentSessionData['streams'].iteritems():
@@ -59,7 +72,7 @@ for username in usernames:
                     measurement_type = streamdata['measurement_type']
 
                     # Then print some debugging info
-                    print("[" + str(datetime.now()) + "] Entering data from stream " + streamname + " (" + measurement_type+ ") into database...")
+                    debugMessage("[" + str(datetime.now()) + "] Entering data from stream " + streamname + " (" + measurement_type+ ") into database...")
 
                     # Set the rest of the values
                     average_value = streamdata['average_value']
@@ -68,14 +81,17 @@ for username in usernames:
 
                     measurementCount = 1
 
-                    print("[" + str(datetime.now()) + "] Found " + str((streamdata['size'])) + " records in this stream.")
+                    debugMessage("[" + str(datetime.now()) + "] Found " + str((streamdata['size'])) + " records in this stream.")
+
+
                     # For each specific measurement within the stream
                     for measurement in streamdata['measurements']:
 
                         # Print the status of record entry in database.
-                        print ("\rEntering record " + str(measurementCount) + " of " + str((streamdata['size'])) + " into database..."),
+                        print("\rEntering record " + str(measurementCount) + " of " + str((streamdata['size'])) + " into database..."),
                         if (measurementCount == streamdata['size']):
-                            print("\r[" + str(datetime.now()) + "] Finished entering records for stream.")
+                            print("\r")
+                            debugMessage("[" + str(datetime.now()) + "] Finished entering records for stream.")
                         measurementCount += 1
 
                         # Set the values for the measurement
@@ -90,7 +106,8 @@ for username in usernames:
                             dbConnection.autocommit = True
 
                         except:
-                            print("[" + str(datetime.now()) + "] Error connecting to database...")
+                            debugMessage("[" + str(datetime.now()) + "] Error connecting to database...")
+
                             sys.exit(-1)
 
                         try:
@@ -102,9 +119,10 @@ for username in usernames:
                         except psycopg2.Error as e:
                             # No need to log duplicity errors
                             if ("23505" not in e.pgcode):
-                                print("[" + str(datetime.now()) + "] An error occurred entering data into database. Details:\n" + e.pgerror)
+                                debugMessage("[" + str(datetime.now()) + "] An error occurred entering data into database. Details:\n" + e.pgerror)
 
             except:
-                print("[" + str(datetime.now()) + "] An error occurred requesting or processing data from session " + str(currentID) + " of " + username)
+                debugMessage("[" + str(datetime.now()) + "] An error occurred requesting or processing data from session " + str(currentID) + " of " + username)
+
     except:
-        print("[" + str(datetime.now()) + "] An error occurred requesting sessions for " + username + " from day " + str(yesterday_day_of_year) + " to " + str(today_day_of_year))
+        debugMessage("[" + str(datetime.now()) + "] An error occurred requesting sessions for " + username + " from day " + str(yesterday_day_of_year) + " to " + str(today_day_of_year))
