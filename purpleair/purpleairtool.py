6ffdabf6bc75356b1b87ebe8aa6294cf06fe9704
George W. Walker <george@georgewwalker.com>
2017-10-05 07:30:35 -0500

    Update new Purple Air tool

--- purpleair/purpleairtool.py
+++ purpleair/purpleairtool.py
@@ -1,12 +1,9 @@
 # This file processes a JSON file to return a list of needed values for Purple Air devices
-# TODO: Add the Lat/Long to the stationarylocations table, enter directly to database instead of pgloader.
+# TODO: Check previous days
 
 import json, csv, urllib, sys, os, psycopg2, pytz
 from datetime import datetime
 import keys
-
-# Timezone to fetch the data using
-timezone = "America/Chicago"
 
 # Open the log file
 log = open("/data/sasa_airquality/airbeamterrier/logs/airbeamterrier.log", "a+")
@@ -33,17 +30,43 @@ decoded_response = JSONresponse.decode('iso-8859-1').encode('utf8')
 # Parse the JSON
 JSONdata = json.loads(decoded_response)
 
+# Connect to the database
+try:
+    dbConnection = psycopg2.connect(host=keys.hostname, user=keys.username, password=keys.password, dbname=keys.database)
+    dbConnection.autocommit = True
+    dbCursor = dbConnection.cursor()
+
+except:
+    debugMessage("Error connecting to database...");
+    sys.exit(-1)
+
 # For each "result" in the JSON
 for result in JSONdata['results']:
     # First, check that it has a label, otherwise it might fail here
     if (result['Label']):
+        # Clean up the label and make uppercase
+        fixed_label = upper(result['Label']).replace(" ","").replace("-","_");
         # If the label starts with SASA
-        if (upper(result['Label']).startswith("SASA")):
+        if (fixed_label).startswith("SASA")):
 
             thingspeak_id = result['THINGSPEAK_PRIMARY_ID']
             thingspeak_apikey = result['THINGSPEAK_PRIMARY_ID_READ_KEY']
 
-            #TODO: Check what days have executed (use a database call?) and look at any other days (past an initial date?). Once a day has been looked at, mark it as looked at somehow (another database call?) and use this to set the start_date and end_date
+            # Reset community
+            community = ''
+
+            # If size is correct to contain a community
+            if (len(sensorname) >= 10):
+                # Get the community from the 3rd spot between _
+                community = sensorname.split("_")[2]
+
+            #TODO: Check what days have executed (use a database call?) and look at any other days (past an initial date?). Once a day has been looked at, mark it as looked at somehow (another database call?) and use this to set the start_date and end_date for below
+
+            # The day to look at
+            start_date = ''
+
+            # The next day (not inclusive)
+            end_date = ''
 
             CSVurl = "https://thingspeak.com/channels/" + thingspeak_id + "/feed.csv?api_key=" + thingspeak_apikey + "&offset=0&average=&round=2&start=" + start_date + "&end=" + end_date + "&timezone=" + timezone
 
@@ -55,27 +78,24 @@ for result in JSONdata['results']:
             checkedHeader = False
 
             # For each row in the CSV
-            for row in parsedCSV:
+            for row in CSVdata:
+
+                # Convert time string to datetime object (format in CSV: "2017-08-02 23:34:45 UTC"):
+                time = datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S UTC')
+
+                # Set timezone on object (set via offset parameter, we are using UTC in request)
+                UTC = pytz.timezone('UTC')
+                time = UTC.localize(time)
+
+                # Adjust datetime to Chicago timezone
+                time = time.astimezone(pytz.timezone("America/Chicago"))
 
                 if (checkedHeader is True):
                     # This should be all the rows past the header.
                     #TODO: Enter the data into the database here
+                    dbCursor.execute("INSERT INTO purpleairprimary (created_at, entry_id, pm1_cf_atm_ugm3, pm25_cf_atm_ugm3, pm10_cf_atm_ugm3, uptimeminutes, rssi_dbm, temperature_f, humidity_percent, pm25_cf_1_ugm3, device_name, community) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9], fixed_label, community)
 
 
                 # Skip the header
                 else:
                     checkedHeader = True
-
-
-            # # Call the shell script to download the data
-            # call(["sh", "/data/sasa_airquality/purpleair/curltool.sh", thingspeak_id, thingspeak_apikey, outputfilename])
-
-
-
-
-
-            # # Write a section of the pgloader file for this item
-            # pgloaderfile.write("LOAD CSV\n    FROM 'new" + outputfilename + ".csv' WITH ENCODING iso-646-us\n    INTO postgresql://postgres@localhost:5432/sasa_airquality?purpleairprimary\n    WITH  skip header = 2,\n        fields optionally enclosed by '\"',\n        fields escaped by backslash-quote,\n        fields terminated by ','\n\n    SET work_mem to '32 MB', maintenance_work_mem to '64 MB';\n\n")
-
-# Close the pgloaderfile
-pgloaderfile.close()
