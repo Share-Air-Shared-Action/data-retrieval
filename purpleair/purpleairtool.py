4e509bb8d198084058cd44ca4bd6f1222693580a
George W. Walker <george@georgewwalker.com>
2017-10-09 21:16:48 -0500

    Finish PurpleAir code

--- purpleair/purpleairtool.py
+++ purpleair/purpleairtool.py
@@ -1,8 +1,7 @@
 # This file processes a JSON file to return a list of needed values for Purple Air devices
-# TODO: Check previous days
 
-import json, csv, urllib, sys, os, psycopg2, pytz
-from datetime import datetime
+import json, csv, urllib, sys, os, StringIO, psycopg2
+from datetime import datetime, timedelta
 import keys
 
 # Open the log file
@@ -21,6 +20,15 @@ JSONurl = 'https://map.purpleair.org/json'
 # Set the download directory
 downloadDirectory = '/data/sasa_airquality/purpleair/downloads/'
 
+# Set the initial date
+initialDate = datetime.strptime("2017-03-20","%Y-%m-%d")
+
+# Get the current date
+currentDate = datetime.today()
+
+# Set the amount of time to change with each iteration
+delta = timedelta(days=1)
+
 # Get the JSON from the URL
 JSONresponse = urllib.urlopen(JSONurl).read()
 
@@ -36,66 +44,120 @@ try:
     dbConnection.autocommit = True
     dbCursor = dbConnection.cursor()
 
+except (KeyboardInterrupt, SystemExit):
+    sys.exit(-1)
 except:
     debugMessage("Error connecting to database...");
-    sys.exit(-1)
+    sys.exit(-1000)
 
-# For each "result" in the JSON
-for result in JSONdata['results']:
-    # First, check that it has a label, otherwise it might fail here
-    if (result['Label']):
-        # Clean up the label and make uppercase
-        fixed_label = upper(result['Label']).replace(" ","").replace("-","_");
-        # If the label starts with SASA
-        if (fixed_label).startswith("SASA")):
+debugMessage("Starting purpleair tool.")
 
-            thingspeak_id = result['THINGSPEAK_PRIMARY_ID']
-            thingspeak_apikey = result['THINGSPEAK_PRIMARY_ID_READ_KEY']
+# For each date since the initial date
+d = initialDate
+while d <= currentDate:
 
-            # Reset community
-            community = ''
+    dString = d.strftime("%Y-%m-%d")
 
-            # If size is correct to contain a community
-            if (len(sensorname) >= 10):
-                # Get the community from the 3rd spot between _
-                community = sensorname.split("_")[2]
+    # Check if the date has run before
+    dbCursor.execute("SELECT date FROM purpleairdone where date = %s",(dString,))
 
-            #TODO: Check what days have executed (use a database call?) and look at any other days (past an initial date?). Once a day has been looked at, mark it as looked at somehow (another database call?) and use this to set the start_date and end_date for below
+    # This skips the current loop if the date has run before
+    if (dbCursor.fetchone() is not None):
+        debugMessage("Skipping date '" + dString + "' as it has run before.")
+        d += delta
+        continue
 
-            # The day to look at
-            start_date = ''
+    debugMessage("Running for date '" + dString + "'")
 
-            # The next day (not inclusive)
-            end_date = ''
+    # For each "result" in the JSON
+    for result in JSONdata['results']:
+        # First, check that it has a label, otherwise it might fail here
+        if (result['Label']):
+            # Clean up the label and make uppercase
+            fixed_label = result['Label'].upper().replace(" ","").replace("-","_");
+            # If the label starts with SASA
+            if (fixed_label).startswith("SASA"):
 
-            CSVurl = "https://thingspeak.com/channels/" + thingspeak_id + "/feed.csv?api_key=" + thingspeak_apikey + "&offset=0&average=&round=2&start=" + start_date + "&end=" + end_date + "&timezone=" + timezone
+                debugMessage("Requesting data for '" + fixed_label + "' during the date '" + dString + "'")
 
-            CSVresponse = urllib.urlopen(CSVurl).read()
+                # Get the API endpoint ID and key
+                thingspeak_id = result['THINGSPEAK_PRIMARY_ID']
+                thingspeak_apikey = result['THINGSPEAK_PRIMARY_ID_READ_KEY']
 
-            CSVdata = csv.reader(CSVresponse, delimiter=',')
+                # Reset community
+                community = ''
 
-            # Skip the header by using this variable
-            checkedHeader = False
+                # If size is correct to contain a community
+                if (len(fixed_label) >= 10):
+                    # Get the community from the 3rd spot between _
+                    community = fixed_label.split("_")[2]
 
-            # For each row in the CSV
-            for row in CSVdata:
+                # The day to look at (current date in loop)
+                start_date = d
 
-                # Convert time string to datetime object (format in CSV: "2017-08-02 23:34:45 UTC"):
-                time = datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S UTC')
+                # The next day (data is not inclusive, go up to the current day)
+                end_date = d + delta
 
-                # Set timezone on object (set via offset parameter, we are using UTC in request)
-                UTC = pytz.timezone('UTC')
-                time = UTC.localize(time)
+                try:
+                    CSVurl = "https://thingspeak.com/channels/" + thingspeak_id + "/feed.csv?api_key=" + thingspeak_apikey + "&offset=0&average=&round=2&start=" + start_date.strftime("%Y-%m-%d") + "&end=" + end_date.strftime("%Y-%m-%d")
 
-                # Adjust datetime to Chicago timezone
-                time = time.astimezone(pytz.timezone("America/Chicago"))
+                    # debugMessage(CSVurl)
 
-                if (checkedHeader is True):
-                    # This should be all the rows past the header.
-                    #TODO: Enter the data into the database here
-                    dbCursor.execute("INSERT INTO purpleairprimary (created_at, entry_id, pm1_cf_atm_ugm3, pm25_cf_atm_ugm3, pm10_cf_atm_ugm3, uptimeminutes, rssi_dbm, temperature_f, humidity_percent, pm25_cf_1_ugm3, device_name, community) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9], fixed_label, community)
+                    CSVresponse = urllib.urlopen(CSVurl).read()
+                    CSVdata = csv.reader(StringIO.StringIO(CSVresponse), delimiter=',')
 
+                except (KeyboardInterrupt, SystemExit):
+                    sys.exit(-1)
+                except:
+                    debugMessage("WARNING: Error downloading or parsing CSV for '" + fixed_label + "' during the date (" + d.strftime("%Y-%m-%d") + "). Skipping.")
+                    continue
 
-                # Skip the header
-                else:
-                    checkedHeader = True
+                # Skip the header by using this variable
+                checkedHeader = False
+
+                # For each row in the CSV
+                for row in CSVdata:
+                    if (checkedHeader is True) and (row):
+
+                        # Psycopg2 throws an error on empty strings in non string fields, so make NULL instead
+                        if (not row[0]):
+                            row[0] = None
+                        if (not row[1]):
+                            row[1] = None
+                        if (not row[2]):
+                            row[2] = None
+                        if (not row[3]):
+                            row[3] = None
+                        if (not row[4]):
+                            row[4] = None
+                        if (not row[5]):
+                            row[5] = None
+                        if (not row[6]):
+                            row[6] = None
+                        if (not row[7]):
+                            row[7] = None
+                        if (not row[8]):
+                            row[8] = None
+                        if (not row[9]):
+                            row[9] = None
+
+                        # This should be all the rows past the header, so enter them into database
+                        try:
+                            dbCursor.execute("INSERT INTO purpleairprimary (created_at, entry_id, pm1_cf_atm_ugm3, pm25_cf_atm_ugm3, pm10_cf_atm_ugm3, uptimeminutes, rssi_dbm, temperature_f, humidity_percent, pm25_cf_1_ugm3, device_name, community) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9], fixed_label, community))
+                        except (KeyboardInterrupt, SystemExit):
+                            sys.exit(-1)
+                        except psycopg2.Error as e:
+                            # No need to log duplicity errors
+                            if ("23505" not in e.pgcode):
+                                debugMessage("[" + str(datetime.now()) + "] An error occurred entering data into database. Details:\n" + e.pgerror)
+                                sys.exit(-1)
+
+                    # Skip the header
+                    else:
+                        checkedHeader = True
+
+    # Log that this day has completed
+    dbCursor.execute("INSERT INTO purpleairdone(date) VALUES (%s)", (dString,))
+
+    # Go to next day
+    d += delta
