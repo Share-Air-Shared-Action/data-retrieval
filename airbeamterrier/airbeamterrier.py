991aa7d6cde964ebb3b278e755756b67470abb23
George W. Walker <george@georgewwalker.com>
2017-10-22 13:46:06 -0500

    New Airbeam/Terrier code
    Now keeps track of what runs have been entered, and checks every day since the start date for new runs in case they are added more than a day after they are completed.

--- airbeamterrier/airbeamterrier.py
+++ airbeamterrier/airbeamterrier.py
@@ -1,135 +1,169 @@
 # This file gets a list of sessions from the Aircasting API, then downloads the JSON for each session from the API, parses it and puts it into the database
 
 import json, urllib, sys, os, psycopg2, pytz
-from datetime import datetime
+from datetime import datetime, timedelta
 import keys
 
 # Open the log file
-f = open("/data/sasa_airquality/airbeamterrier/logs/airbeamterrier.log", "a+")
+log = open("/data/sasa_airquality/airbeamterrier/logs/airbeamterrier.log", "a+")
+
+# Set the initial date
+initialDate = datetime.strptime("2017-04-01","%Y-%m-%d")
+
+# Get the current date
+currentDate = datetime.today()
+
+# Set the amount of time to change with each iteration
+delta = timedelta(days=1)
 
 # Create a function for logging/displaying output
 def debugMessage(message):
     print(message)
-    f.write(message + "\n")
-    f.flush()
-    os.fsync(f.fileno())
+    log.write(message + "\n")
+    log.flush()
+    os.fsync(log.fileno())
 
 # Set the "usernames" for the API call
 # These may be changed if they add a new phone
 usernames = ["SASA_AB1", "SASA_AB2", "SASA_AB3", "SASA_AB4", "SASA_AB5", "SASA_AB6", "SASA_AB7", "SASA_AB8", "SASA_AB9"]
 
-# Get the current day of the year
-today_day_of_year = datetime.now().timetuple().tm_yday
-
-# Get 2 days ago day of the year
-yesterday_day_of_year = today_day_of_year - 2
-
 # Set the timezone
 tz = pytz.timezone("America/Chicago")
 
-for username in usernames:
-    allSessionsURL = "http://aircasting.org/api/sessions.json?page=0&page_size=50&q%5Btime_from%5D=0&q%5Btime_to%5D=2359&q%5Bday_from%5D=" + str(yesterday_day_of_year) + "&q%5Bday_to%5D=" + str(today_day_of_year) + "&q%5Busernames%5D=" + username + "&q%5Blocation%5D=Chicago&q%5Bunit_symbol%5D=%C2%B5g%2Fm"
+# Connect to the database
+try:
+    dbConnection = psycopg2.connect(host=keys.hostname, user=keys.username, password=keys.password, dbname=keys.database)
+    dbConnection.autocommit = True
 
-    debugMessage("[" + str(datetime.now()) + "] Requesting sessions for " + username + " from day " + str(yesterday_day_of_year) + " to " + str(today_day_of_year))
+    dbCursor = dbConnection.cursor()
 
-    try:
-        # Get the JSON from the URL
-        allSessionsResponse = urllib.urlopen(allSessionsURL).read()
+except (KeyboardInterrupt, SystemExit):
+    debugMessage("[" + str(datetime.now()) + "] Received keyboard interrupt or other system exit signal. Quitting.")
+    sys.exit(-1)
+except Exception as e:
+    debugMessage("[" + str(datetime.now()) + "] Error connecting to database. Details: " + str(e))
 
-        # Parse the JSON
-        allSessionsData = json.loads(allSessionsResponse)
+    sys.exit(-1)
 
-        # Debugging message
-        debugMessage("[" + str(datetime.now()) + "] Got " + str(len(allSessionsData)) + " sessions.")
+# For each date since the initial date
+d = initialDate
+while d <= currentDate:
 
-        sessionCount = 1
+    # Set the date data for this iteration
+    end_date = d + delta
+    startString = d.timetuple().tm_yday
+    endString = end_date.timetuple().tm_yday
 
-        # For each entry in the JSON
-        for item in allSessionsData:
+    for username in usernames:
+        allSessionsURL = "http://aircasting.org/api/sessions.json?page=0&page_size=50&q%5Btime_from%5D=0&q%5Btime_to%5D=2359&q%5Bday_from%5D=" + str(startString) + "&q%5Bday_to%5D=" + str(endString) + "&q%5Busernames%5D=" + username + "&q%5Blocation%5D=Chicago&q%5Bunit_symbol%5D=%C2%B5g%2Fm"
 
-            # Get its ID
-            currentID = item['id']
+        debugMessage("[" + str(datetime.now()) + "] Requesting sessions for " + username + " from day " + str(startString) + " to " + str(endString))
 
-            # Time to request the JSON for the specific ID
-            sessionURL = "http://aircasting.org/api/sessions/" + str(currentID) + ".json"
+        try:
+            # Get the JSON from the URL
+            allSessionsResponse = urllib.urlopen(allSessionsURL).read()
 
-            debugMessage("[" + str(datetime.now()) + "] [" + str(sessionCount) + " of " + str(len(allSessionsData)) + "] Requesting data from session " + str(currentID) + " of " + username)
+            # Parse the JSON
+            allSessionsData = json.loads(allSessionsResponse)
 
-            sessionCount += 1
+            # Debugging message
+            debugMessage("[" + str(datetime.now()) + "] Got " + str(len(allSessionsData)) + " sessions.")
 
-            try:
-                # Get the JSON from the URL
-                currentSessionResponse = urllib.urlopen(sessionURL).read()
+            sessionCount = 1
 
-                # Parse the JSON
-                currentSessionData = json.loads(currentSessionResponse)
+            # For each entry in the JSON
+            for item in allSessionsData:
 
-                # Set the session title
-                session_title = currentSessionData['title'].strip()
+                # Get its ID
+                currentID = item['id']
 
-                debugMessage("[" + str(datetime.now()) + "] Session data found. Session title: " + session_title)
+                # Time to request the JSON for the specific ID
+                sessionURL = "http://aircasting.org/api/sessions/" + str(currentID) + ".json"
 
-                # For each stream within the current session
-                for streamname, streamdata in currentSessionData['streams'].iteritems():
+                debugMessage("[" + str(datetime.now()) + "] [" + str(sessionCount) + " of " + str(len(allSessionsData)) + "] Requesting data from session " + str(currentID) + " of " + username)
 
-                    # Set the measurement type first
-                    measurement_type = streamdata['measurement_type']
+                sessionCount += 1
 
-                    # Then print some debugging info
-                    debugMessage("[" + str(datetime.now()) + "] Entering data from stream " + streamname + " (" + measurement_type+ ") into database...")
+                try:
+                    # Get the JSON from the URL
+                    currentSessionResponse = urllib.urlopen(sessionURL).read()
 
-                    # Set the rest of the values
-                    average_value = streamdata['average_value']
-                    sensor_package_name = streamdata['sensor_package_name']
-                    unit_name = streamdata['unit_name']
+                    # Parse the JSON
+                    currentSessionData = json.loads(currentSessionResponse)
 
-                    measurementCount = 1
+                    # Set the session title
+                    session_title = currentSessionData['title'].strip()
 
-                    debugMessage("[" + str(datetime.now()) + "] Found " + str((streamdata['size'])) + " records in this stream.")
+                    # Check if this session_title has been entered
+                    dbCursor.execute("SELECT session_title FROM airterrierdone where session_title = %s",(session_title,))
 
+                    # This skips the current loop if the session_title has been entered before
+                    if (dbCursor.fetchone() is not None):
+                        debugMessage("[" + str(datetime.now()) + "] Skipping session_title '" + session_title + "' as it has already been entered.")
+                        continue
 
-                    # For each specific measurement within the stream
-                    for measurement in streamdata['measurements']:
+                    debugMessage("[" + str(datetime.now()) + "] New session data found. Session title: " + session_title)
 
-                        # Print the status of record entry in database.
-                        print("\rEntering record " + str(measurementCount) + " of " + str((streamdata['size'])) + " into database..."),
-                        if (measurementCount == streamdata['size']):
-                            print("\r")
-                            debugMessage("[" + str(datetime.now()) + "] Finished entering records for stream.")
-                        measurementCount += 1
+                    # For each stream within the current session
+                    for streamname, streamdata in currentSessionData['streams'].iteritems():
 
-                        # Set the values for the measurement
-                        latitude = measurement['latitude']
-                        longitude = measurement['longitude']
+                        # Set the measurement type first
+                        measurement_type = streamdata['measurement_type']
 
-                        # Add the timezone to the time (conver from string to datetime object first)
-                        time = tz.localize(datetime.strptime(measurement['time'], '%Y-%m-%dT%H:%M:%SZ'))
+                        # Then print some debugging info
+                        debugMessage("[" + str(datetime.now()) + "] Entering data from stream " + streamname + " (" + measurement_type+ ") into database...")
 
-                        measured_value = measurement['value']
+                        # Set the rest of the values
+                        average_value = streamdata['average_value']
+                        sensor_package_name = streamdata['sensor_package_name']
+                        unit_name = streamdata['unit_name']
 
-                        # Finally, we need to enter this data into the database
-                        try:
-                            dbConnection = psycopg2.connect(host=keys.hostname, user=keys.username, password=keys.password, dbname=keys.database)
-                            dbConnection.autocommit = True
+                        measurementCount = 1
 
-                        except:
-                            debugMessage("[" + str(datetime.now()) + "] Error connecting to database...")
+                        debugMessage("[" + str(datetime.now()) + "] Found " + str((streamdata['size'])) + " records in this stream.")
 
-                            sys.exit(-1)
+                        # For each specific measurement within the stream
+                        for measurement in streamdata['measurements']:
 
-                        try:
-                            dbCursor = dbConnection.cursor()
+                            # Print the status of record entry in database.
+                            print("\rEntering record " + str(measurementCount) + " of " + str((streamdata['size'])) + " into database..."),
+                            if (measurementCount == streamdata['size']):
+                                print("\r")
+                                debugMessage("[" + str(datetime.now()) + "] Finished entering records for stream.")
+                            measurementCount += 1
 
-                            dbCursor.execute("""INSERT INTO airterrier (session_title, username, average_value, measurement_type, sensor_package_name, unit_name, latitude, longitude, measured_value, time)
-                                             VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""",(session_title, username, average_value, measurement_type, sensor_package_name, unit_name, latitude, longitude, measured_value, time))
+                            # Set the values for the measurement
+                            latitude = measurement['latitude']
+                            longitude = measurement['longitude']
 
-                        except psycopg2.Error as e:
-                            # No need to log duplicity errors
-                            if ("23505" not in e.pgcode):
-                                debugMessage("[" + str(datetime.now()) + "] An error occurred entering data into database. Details:\n" + e.pgerror)
+                            # Add the timezone to the time (conver from string to datetime object first)
+                            time = tz.localize(datetime.strptime(measurement['time'], '%Y-%m-%dT%H:%M:%SZ'))
 
-            except:
-                debugMessage("[" + str(datetime.now()) + "] An error occurred requesting or processing data from session " + str(currentID) + " of " + username)
+                            measured_value = measurement['value']
 
-    except:
-        debugMessage("[" + str(datetime.now()) + "] An error occurred requesting sessions for " + username + " from day " + str(yesterday_day_of_year) + " to " + str(today_day_of_year))
+                            try:
+                                dbCursor.execute("""INSERT INTO airterrier (session_title, username, average_value, measurement_type, sensor_package_name, unit_name, latitude, longitude, measured_value, time)
+                                                 VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""",(session_title, username, average_value, measurement_type, sensor_package_name, unit_name, latitude, longitude, measured_value, time))
+                            except (KeyboardInterrupt, SystemExit):
+                                debugMessage("[" + str(datetime.now()) + "] Received keyboard interrupt or other system exit signal. Quitting.")
+                                sys.exit(-1)
+                            except psycopg2.Error as e:
+                                # No need to log duplicity errors
+                                if ("23505" not in e.pgcode):
+                                    debugMessage("[" + str(datetime.now()) + "] An error occurred entering data into database. Details:\n" + e.pgerror)
+
+                    # Log that this session_title has completed
+                    dbCursor.execute("INSERT INTO airterrierdone(session_title) VALUES (%s)", (session_title,))
+                except (KeyboardInterrupt, SystemExit):
+                    debugMessage("[" + str(datetime.now()) + "] Received keyboard interrupt or other system exit signal. Quitting.")
+                    sys.exit(-1)
+                except Exception as e:
+                    debugMessage("[" + str(datetime.now()) + "] An error occurred requesting or processing data from session " + str(currentID) + " of " + username + ". Details: " + str(e))
+        except (KeyboardInterrupt, SystemExit):
+            debugMessage("[" + str(datetime.now()) + "] Received keyboard interrupt or other system exit signal. Quitting.")
+            sys.exit(-1)
+        except Exception as e:
+            debugMessage("[" + str(datetime.now()) + "] An error occurred requesting sessions for " + username + " from day " + str(startString) + " to " + str(endString) + ". Details: " + str(e))
+
+    # Go to next day
+    d += delta
