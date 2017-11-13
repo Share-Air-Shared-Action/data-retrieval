f42e4eb1013993e15f07a92fc09d5c99e302e40d
pkmnct <george@georgewwalker.com>
2017-11-13 09:04:56 -0600

    Create stationarylocations.sql
    Need to add other manually-added stationary locations still.

---
+++ stationarylocations.sql
@@ -0,0 +1,19 @@
+-- Build stationarylocations
+
+INSERT INTO stationarylocations(unit_id, latitude, longitude, community)
+  VALUES('SASA_AQ1', 42.13999624, -87.79922692, 'NB');
+
+INSERT INTO stationarylocations(unit_id, latitude, longitude, community)
+  VALUES('SASA_AQ2', 42.13999623, -87.79922692, 'NB');
+
+INSERT INTO stationarylocations(unit_id, latitude, longitude, community)
+  VALUES('SASA_AQ1', 41.8609802, -87.6280025, 'SL');
+
+INSERT INTO stationarylocations(unit_id, latitude, longitude, community)
+  VALUES('SASA_AQ2', 41.8609802, -87.6280025, 'SL');
+
+INSERT INTO stationarylocations(unit_id, latitude, longitude, community)
+  VALUES('SASA_AQ1', 41.653887, -87.602826, 'PC');
+
+INSERT INTO stationarylocations(unit_id, latitude, longitude, community)
+  VALUES('SASA_AQ2', 41.653887, -87.602826, 'PC');
