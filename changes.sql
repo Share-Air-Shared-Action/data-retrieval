34dc59a4b279959b5de4a3b3084414b6f8626da4
George W. Walker <george@georgewwalker.com>
2017-12-22 11:30:55 -0600

    Additional renames

--- changes.sql
+++ changes.sql
@@ -6,6 +6,7 @@
 update airterrier set session_title='test_pc1_0726pm' where session_title='pc1_0726pm';
 update airterrier set session_title='LV7_0706PM' where session_title='Queen Elizabeth Session';
 update airterrier set session_title='LV8_0622PM' where session_title='LV8_0622';
+update airterrier set session_title='test_pc1_0727am' where session_title='pc1_0806am';
 
 -- Renames on PC per PCRAirMonitoringReview.xlsx
 update airterrier set session_title='PC1_0824AM' where session_title='pc1_0824_2017';
@@ -34,3 +35,8 @@ update airterrier set session_title='PC4_0821AM' where session_title='pc4_0821pm
 update airterrier set session_title='PC4_0823PM' where session_title='pc4_0823pm';
 update airterrier set session_title='PC4_0825PM' where session_title='pc4_0825pm';
 update airterrier set session_title='PC4_0830PM' where session_title='pc4_0830_am';
+
+
+-- Fix Purple Air that wasn't re-registered
+update purpleairprimary set device_name='SASA_PA2_PC_S' where device_name='SASA_PA6_SE_S6' and created_at > '2017-08-10 00:00:00-05';
+update purpleairprimary set device_name='SASA_PA2_PC_SB' where device_name='SASA_PA6_SE_S6B' and created_at > '2017-08-10 00:00:00-05';
