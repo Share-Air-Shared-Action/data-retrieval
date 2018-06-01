39a8f94424eb610e2dd4d860ed05f478f8513d2d
vijaykumardev <vijayvk.it@gmail.com>
2018-05-31 22:42:57 -0600

    Data cleaning on purpleairprimary data

--- triggers.sql
+++ triggers.sql
@@ -98,8 +98,11 @@ update metone set type='pm10' where type='conc' and unit_id='SASA_MO1' and commu
 update purpleairprimary set error=1 where community='NB' and to_char(created_at,'YYYY')='2018' and error is distinct from 1;
 update purpleairprimary set error=1 where device_name in (select device_name from (select count(*) cnt,device_name from purpleairprimary where to_char(created_at, 'YYYY-MM') = '2017-12' group by device_name) tb where cnt<200) and to_char(created_at, 'YYYY-MM') = '2017-12' and error is distinct from 1;
 
-
-
 ---Check for runs in Airterrier table that are less than a minute and remove 
 UPDATE airterrier SET error=1 WHERE session_title IN (select session_title from airterrier group by session_title having EXTRACT(EPOCH FROM (max(time) - min(time))) < 60 ) and error is distinct from 1;
 
+-- The device was turned on October for few minutes
+update purpleairprimary set error=1 where device_name='SASA_PA9_SE_S4' and date(created_at)> date('2017-08-09');
+
+-- removing stray readings on March 9th
+update purpleairprimary set error=1 where device_name='SASA_PA2_SL_W' or 'SASA_PA2_SL_WB' and date(created_at) < date('2017-04-01');
